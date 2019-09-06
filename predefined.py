9484634f50fc61f65edad9de3212a49ae51fe09a
GrigTukhar <48716024+GrigTukhar@users.noreply.github.com>
2019-09-06 12:26:06 +0400

    Create predefined.py

---
+++ predefined.py
@@ -0,0 +1,8 @@
+class Predefined:
+
+    def __init__(self,type,minutes,distance,load):
+        self.type = type
+        self.minutes = minutes
+        self.distance = distance
+        self.load = load
+
