812900a66688ff7ce9e325a5125df2f7529ed8e8
GrigTukhar <48716024+GrigTukhar@users.noreply.github.com>
2019-09-25 00:50:35 +0400

    Delete predefined.py

--- predefined.py
+++
@@ -1,8 +0,0 @@
-class Predefined:
-
-    def __init__(self,type,minutes,distance,load):
-        self.type = type
-        self.minutes = minutes
-        self.distance = distance
-        self.load = load
-
